ccd01a2190b5785371f347d415ad5ac6294dc618
firstname lastname <ivan.petrushin1982@gmail.com>
2020-02-03 19:04:41 +0400

    lesson 22-63

---
+++ counter-app/src/components/navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Navbar = ({totalCounters}) => {
+    return (
+        <div>
+            <nav className="navbar navbar-light bg-light">
+                <a className="navbar-brand" href="#">
+                    Navbar {" "}
+                    <span
+                        className="badge badge-secondary badge-pill badge-sm">
+                        {totalCounters}
+                    </span>
+                </a>
+            </nav>
+        </div>
+    );
+};
+
+export default Navbar;
--- vidly/src/index.js
+++ vidly/src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';   
-import 'bootstrap/dist/css/bootstrap.css'
+import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css'
 
 ReactDOM.render(<App />, document.getElementById('root'));
